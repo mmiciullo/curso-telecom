9d38a83c932e08efe895037cda4f74b06e4a0343
mmiciullo <mmiciullo@gmail.com>
2022-06-28 23:36:16 -0300

    ejercicio 1 y 2

---
+++ Modulo 12/Ejercicio 1/script/script.js
@@ -0,0 +1,7 @@
+let edad = prompt('Ingrese su edad')
+
+if (edad > 18){
+    alert('Bienvenido.')
+}else{
+    alert('Usted es menor de edad.')
+}
---
+++ Modulo 12/Ejercicio 2/script/login.js
@@ -0,0 +1,8 @@
+let user = prompt('Ingrese el nombre de usuario')
+let pw = Number(prompt('Ingrese su contraseña'))
+
+if(user == 'admin' && pw == 1234){
+    alert('Bienvenido')
+}else{
+    alert('Usuario o contraseña incorrecta.')
+}
