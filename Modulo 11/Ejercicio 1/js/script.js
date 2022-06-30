781fb98245131e4d8a03d98056b0afdd0d34b604
mmiciullo <mmiciullo@gmail.com>
2022-06-29 21:48:40 -0300

    changes

--- Modulo 11/Ejercicio 1/js/script.js
+++ Modulo 11/Ejercicio 1/js/script.js
@@ -1,6 +1,6 @@
-let cFijo = prompt("Ingrese el costo fijo");
-let pVenta = prompt("Ingrese el precio de venta");
-let cVariables = prompt("Ingrese los costos variables");
+let cFijo = parseInt(prompt("Ingrese el costo fijo"));
+let pVenta = parseInt(prompt("Ingrese el precio de venta"));
+let cVariables = parseInt(prompt("Ingrese los costos variables"));
 
 let result = cFijo / (pVenta - cVariables);
 
