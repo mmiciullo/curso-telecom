d3e338a283bd2053b9612e61d6293bf6d731af9d
mmiciullo <mmiciullo@gmail.com>
2022-07-10 18:44:30 -0300

    delete files

--- Modulo 12/Ejercicio 1/script/script.js
+++
@@ -1,7 +0,0 @@
-let edad = prompt('Ingrese su edad')
-
-if (edad > 18){
-    alert('Bienvenido.')
-}else{
-    alert('Usted es menor de edad.')
-}
--- Modulo 12/Ejercicio 2/script/login.js
+++
@@ -1,8 +0,0 @@
-let user = prompt('Ingrese el nombre de usuario')
-let pw = prompt('Ingrese su contraseña')
-
-if(user == 'admin' && pw == 1234){
-    alert('Bienvenido')
-}else{
-    alert('Usuario o contraseña incorrecta.')
-}
