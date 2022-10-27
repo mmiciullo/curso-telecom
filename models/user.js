52ee5e05841d7cc6379378f242fb964b88e74034
mmiciullo <mmiciullo@gmail.com>
2022-10-27 13:02:45 -0300

    Registrar usuario

--- models/user.js
+++ models/user.js
@@ -1,51 +1,39 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt')
-const SALT_WORK_FACTOR = 10;
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    require: true,
-    unique: true,
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: [true, "username is required"],
+      lowercase: true,
+    },
+    pw: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    // userAvatar: { type: String },
   },
-  password: {
-    type: String,
-    require: true,
-  },
-  email: {
-    type: String,
-    require: true,
-    unique: true,
-  },
+  { versionKey: false }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("pw")) next();
+  this.pw = await bcrypt.hash(this.pw, 10);
+  return next();
 });
 
-
-userSchema.pre("save", function (next) {
-  let user = this;
-
-  // Se hashea la contraseña si ha sido modificada o es nueva
-  if (!user.isModified("password")) return next();
-
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
-
-    // Hash de contraseña usando nuevo  salt
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-      // Reemplazar la contraseña por un hash
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.method.comparePassword = function (password, cb) {
-  bcrypt.compare(password, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
+userSchema.methods.comparePassword = function (passw, cb) {
+  bcrypt.compare(passw, this.password, function (err, isMatch) {
+    if (err) {
+      return cb(err, false);
+    }
+    return cb(null, isMatch);
   });
 };
-
-
-
-
 module.exports = mongoose.model("User", userSchema);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -13,6 +15,16 @@ const userRouter = require("../routes/users");
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    // secret: propiedad para que cada sessión sea guardada de manera única
+    secret: "secretSession",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
 
 // Ruta Principal Home obteniendo los articulos creados
 app.get("/", async (req, res) => {
@@ -22,11 +34,13 @@ app.get("/", async (req, res) => {
   res.render("articles/index", { articles: articles });
 });
 
-// Routes
+// Articles Routes
 app.use("/articles", articleRouter);
-app.use("/users", userRouter);
+app.use("/public", express.static("./public"));
 
-app.use("/public", express.static('./public'));
+// Users Routes
+
+app.use("/users", userRouter);
 
 // MongoDB connection
 mongoose
