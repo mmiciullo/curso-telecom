64d2126593f4e4e9417655835e9780e1a32d3e8f
mmiciullo <50251389+mmiciullo@users.noreply.github.com>
2022-11-02 10:27:40 -0300

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -39,27 +39,3 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
-// passport.use(
-//   new LocalStrategy({ usernameField: "username" }, (username, pw, done) => {
-//     // Matchear usuario por username
-//     User.findOne({
-//       username: username,
-//     }).then((user) => {
-//       if (!user) {
-//         return done(null, false, {
-//           message: "That username is not registered",
-//         });
-//       }
-
-//       // Match password
-//       bcrypt.compare(pw, user.pw, (err, isMatch) => {
-//         if (err) throw err;
-//         if (isMatch) {
-//           return done(null, user);
-//         } else {
-//           return done(null, false, { message: "Password incorrect" });
-//         }
-//       });
-//     });
-//   })
-// );
