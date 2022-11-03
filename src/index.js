8666ca87efeefc946f9be2e8e42124451828b4fd
mmiciullo <mmiciullo@gmail.com>
2022-11-03 10:38:42 -0300

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const methodOverride = require("method-override");
-const session = require("express-session");
+const session = require('express-session')
 const flash = require("connect-flash");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
